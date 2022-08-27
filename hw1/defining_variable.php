adc87e7de522d3f9accd624515e4bfa4e691175a
Sergey Sisharpof <aquadefin@gmail.com>
2022-08-27 22:16:16 +0300

    fix

--- hw1/defining_variable.php
+++ hw1/defining_variable.php
@@ -1,6 +1,6 @@
 <?php
 // Задание №3 - Определение типа переменной.
-$arr = [3.14, 3, 'one', true, null, [], null, []];
+$arr = [3.14, 3, 'one', true, null, []];
 
 foreach ($arr as $i)
 {
@@ -32,12 +32,25 @@ foreach ($arr as $i)
 echo "\n";
 foreach ($arr as $i)
 {
-    echo match ($i) {
-        is_bool($i) => "bool\n",
-        is_float($i) => "float\n",
-        is_int($i) => "int\n",
-        is_string($i) => "string\n",
-        is_null($i) => "null\n",
-        default => "other\n",
-    };
+    switch ($i)
+    {
+        case is_bool($i):
+            echo "bool\n";
+            break;
+        case is_float($i):
+            echo "float\n";
+            break;
+        case is_int($i):
+            echo "int\n";
+            break;
+        case is_string($i):
+            echo "string\n";
+            break;
+        case is_null($i):
+            echo "null\n";
+            break;
+        default:
+            echo "other\n";
+            break;
+    }
 }
